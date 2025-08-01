f0a12768fd90b188060d154546f87132442d2af7
Rico <roberto.ricofernandez@sage.com>
2025-08-01 19:14:46 +0200

    Add dynamic title

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app/app.component";
 import { CurrentDateComponent } from "./app/current-date/current-date.component";
 import { LoaderComponent } from "./app/loader/loader.component";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
-  declarations: [AppComponent, CurrentDateComponent, LoaderComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, LoaderComponent, CurrentDateComponent],
+  imports: [BrowserModule, CommonModule, FormsModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
